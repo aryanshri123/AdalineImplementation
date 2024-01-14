ec7e6061c955e484fd439842b2600cd46d7c768c
Aryan Shrivastava <aryanshrivastava@Aryans-MacBook-Air.local>
2024-01-13 23:23:16 -0600

    updates

--- code/adaline2.py
+++ code/adaline2.py
@@ -47,7 +47,10 @@ class Adaline:
         rand = np.random.RandomState(42)
         self.w = rand.normal(scale=.01, size = self.X.shape[1])
     
-    def _net_inputs(self):
+    def __repr__(self):
+        return str(self.w)
+    
+    def _net_inputs(self, to_test=None):
         '''
         get predicted value according to linear aggregation function.
 
@@ -61,9 +64,12 @@ class Adaline:
         net_input defined as linear combination of weight and feature
         values. 
         '''
-        return np.dot(self.X, self.w)
+        if to_test is None:
+            return np.dot(self.X, self.w)
+        else:
+            return np.dot(to_test, self.w)
 
-    def _threshold(self):
+    def _threshold(self, to_test=None):
         '''
         Threshold function for binary classification.
         Will be defined as such:
@@ -77,25 +83,109 @@ class Adaline:
             vector filled w/ either 1 or -1
                 - takes shape (n, )
         '''
-        net_inputs = self._net_inputs()
-        vec = np.vectorize(lambda x: 1 if x > 0 else -1,)
-        return vec(net_inputs)
+        if to_test is None:
+            net_inputs = self._net_inputs()
+            vec = np.vectorize(lambda x: 1 if x > 0 else -1)
+            return vec(net_inputs)
+        else:
+            net_inputs = self._net_inputs(to_test)
+            vec = np.vectorize(lambda x: 1 if x > 0 else -1)
+            return vec(net_inputs)
     
-    def _loss_func(self):
+    def _sse(self):
         '''
-        Is the loss function for the model. In this case, it will
+        Is an example of a loss function. In this case, it will
         be the sum of squared errors, defined as:
         sum of (target - predicted)^2 for each sample.
 
-        This is mainly a function of the weights
+        This is mainly a function of the weights. 
 
         Input:
             self
         
         Output:
-            err (int): sum of squared errors
+            s (int): sum of squared errors
         '''
         net_inputs = self._net_inputs()
         diff_squared = np.square(self.y - net_inputs)
         return np.sum(diff_squared)
+    
+    def _mse(self):
+        '''
+        Is an example of a loss function. In this case, it will
+        be the mean of squared errors. This is identical to sse,
+        but we divide sum by number of samples. 
 
+        Again, we are mainly interested in the weights
+
+        Input:
+            self
+        
+        Output:
+            m (float): mean of squared errors
+        '''
+        m = self._sse()
+        return m/(self.X.shape[0])
+
+    def _sse_grad(self):
+        '''
+        Get the gradient of the sse loss function with respect to 
+        each weight. This will return a vector of m elements. 
+
+        Input:
+            self
+        
+        Output: 
+            nabla_sse (numpy array):
+                vector of slope of each w_j, (represents gradient of sse)
+        '''
+        # partial derivative of sse with respect to each w_j:
+        # - (sum of (2 * (y - net_input) * x_j)
+        # y is a vector of n elements
+        # net_input is a vector of n elements
+        # x_j is a vector of n elements
+        net_inputs = self._net_inputs()
+        diff = self.y - net_inputs
+        multiplied_diff = 2 * diff
+        
+        nabla_sse = np.array([])
+        for i in range(len(self.w)):
+            curr_feature = self.X.iloc[:, i]
+            result = -np.dot(multiplied_diff, curr_feature)
+            nabla_sse = np.append(nabla_sse, result)
+
+        return nabla_sse
+
+    def _mse_grad(self):
+        '''
+        Get gradient of mse loss function with respect to each weight
+        This will return a vector of m elements.
+
+        Input:
+            self
+        
+        Output:
+            nabla_mse (numpy array):
+                vector of slope of each w_j (represents gradient for mse)
+        '''
+        nabla_sse = self._sse_grad()
+        return nabla_sse/(self.X.shape[0])
+    
+    def train(self, cost_func='mse', learning_rate=1e-3, n_iter=5000):
+        # if cost_func == 'mse':
+        #     gradient = self._mse_grad
+        # else:
+        #     gradient = self._sse_grad
+        for i in range(n_iter):
+            nabla = self._mse_grad()
+            print(nabla)
+            #print(i, learning_rate * nabla, self.w)
+            self.w += (-learning_rate * nabla)
+        
+        # return self.w
+    
+    def predict(self, points):
+        '''
+        point should be an array of m features in correct order
+        '''
+        return self._threshold(points)
--- code/adalineimp.py
+++ code/adalineimp.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import randomdata
+from adaline2 import Adaline
 
 n_samples = 100
 
@@ -25,54 +26,29 @@ full_data = np.r_[alb_data, owl_data]
 df = pd.DataFrame(full_data, columns=['weight', 'wingspan', 'target'])
 df = df.sample(frac = 1, random_state = 42, ignore_index = True)
 
-x = np.full(df.iloc[:, 0].shape, 1)
-df.insert(0, 'bias', x)
-X = df.iloc[:, 0:-1]
-y = df.iloc[:, -1]
-rand = np.random.RandomState(42)
-w = rand.normal(scale=.01, size = X.shape[1])
-ns = np.dot(X, w)
-print(y)
-print(np.sum(np.square(y - ns)))
+
+alb_test = randomdata.species_gen(alb_mean_weight, alb_var_weight, alb_mean_wingspan, alb_var_wingspan, n_samples, alb_target, 142)
+owl_test = randomdata.species_gen(owl_mean_weight, owl_var_weight, owl_mean_wingspan, owl_var_wingspan, n_samples, owl_target, 142)
+
+full_test = np.r_[alb_test, owl_test]
+test_df = pd.DataFrame(full_test, columns=['weight', 'wingspan', 'target'])
+test_df = test_df.sample(frac = 1, random_state = 42, ignore_index = True)
+test_df = test_df.drop('target',axis=1)
+test_df['bias'] = np.full((len(test_df),), 1)
+test_df['target'] = df['weight'].apply(lambda x: 1 if x > 5000 else -1)
+cols = list(test_df.columns)
+cols = ['bias', 'weight', 'wingspan', 'target']
+test_df = test_df[cols]
+
+model = Adaline(df)
+
+def squared_err(row):
+    z = np.dot(row[:-1], model.w)
+    targ = row['target']
+    return np.square(targ - z)
 
 
-
-def initialize_weights(X):
-    '''
-    Initialize x_0 to be 1 because we want a bias term (to be represented by w[0])
-
-
-    '''
-    #return X.shape[0]
-    bias_x = np.full(X.shape[0], 1)
-    #X = np.add(bias, X)
-    X = np.c_[bias_x, X]
-
-    rand = np.random.RandomState(42)
-    w = rand.normal(scale=.01, size=X.shape[1])
-    return X, w
-
-def net_input(X, w):
-    '''
-    Gets prediction y^ (referred to as net input)
-
-    This is just a dot product of the thing
-
-    Returns a vector of net_input
-    '''
-    return np.dot(X, w)
-
-def loss_function(data, w):
-    '''
-    SSE (sum of squared errors)
-    '''
-    X = data[0]
-    y = data[1]
-    # X.shape[1] is num samples
-    return (np.sum(np.square((y - net_input(X,w)))))/(X.shape[0])
-    
-
-# def train(learning_rate, n_iter=100):
-#     for 
+df['net_input'] = model._net_inputs(df.iloc[:, 0:-1])
+print(df['target'] - df['net_input'])
 
 
